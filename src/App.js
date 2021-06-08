57fa5059dc9fe1c4b2dd4a7967407dc0324eec7d
RianneH1990 <74492460+RianneH1990@users.noreply.github.com>
2021-06-08 15:46:15 +0200

    useEffect test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
-  return (
+    let [count, setCount] = useState(0);
 
+    // 2 argumenten
+    // 1e: callback -> wat wil je doen
+    // 2e: dependency array: -> wanneer wil je dit doen
+    // lege dependency rendert alleen op mount. useEffect(() => {}, []), bijvoorbeeld data fetching
+    useEffect(() => {
+        console.log("Leeg dependency array");
+    }, []);
+
+    useEffect(() => {
+        console.log("Voer dit uit wanneer count veranderd!")
+        if (count >= 10) {
+            setCount(0);
+        }
+    }, [count]);
+
+    function addOne() {
+        setCount(count + 1)
+    }
+
+    function setTo0(){
+        setCount(0);
+    }
+
+  return (
+     <div className="App">
+         <header className="App-header">
+             <p>{count}</p>
+             <button onClick={addOne}>+</button>
+             <button onClick={setTo0}>0</button>
+         </header>
+     </div>
   );
 }
 
